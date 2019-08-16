9b934f83fedc0de558fb278b6d56e2979414a89a
Corey Ladovsky <coreyskymusic@gmail.com>
2019-08-16 10:33:57 -0400

    quil

--- frontend/src/Story.js
+++ frontend/src/Story.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function Story(){
     const [text, setText] = useState("")
 
     return (
-      <ReactQuill value={text} onChange={(e) => setText(e.target.value)} />
+      <ReactQuill value={text} onChange={(value) => setText(value)} />
     );
 }
 
