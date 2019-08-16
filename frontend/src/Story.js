21610fff3c0abf3661e0464a2706d7c900ca371b
Corey Ladovsky <coreyskymusic@gmail.com>
2019-08-16 13:51:25 -0400

    frontend submit

--- frontend/src/Story.js
+++ frontend/src/Story.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ReactQuill from "react-quill";
+import axios  from 'axios'
 import "react-quill/dist/quill.snow.css";
 
-function Story(){
-    const [text, setText] = useState("")
+function Story() {
+  const [text, setText] = useState("");
+  const [err, setErr] = useState("")
 
-    return (
-      <ReactQuill value={text} onChange={(value) => setText(value)} />
-    );
+  function handleSubmit(e) {
+      e.preventDefault();
+      try {
+          axios.post("/stories", {story: text})
+      } catch (err) {
+          setErr(err)
+      }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ReactQuill value={text} onChange={value => setText(value)} />
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
 
 export default Story;
